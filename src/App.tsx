a82eb61db1a91426b3c5e2343b74cbedb5a71ec7
nATOr <gowluonade@gmail.com>
2023-06-01 00:58:01 -0500

    lazy-suspense-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { Suspense, lazy } from 'react'
 import { Route } from 'wouter'
 import './App.css'
 import Header from './components/Header'
-import DetailPage from './pages/Detail'
-import TopStoriesPage from './pages/TopStories'
+
+const TopStoriesPage = lazy(() => import('./pages/TopStories'))
+const DetailPage = lazy(() => import('./pages/Detail'))
+
 export default function App () {
   return (
     <>
       <Header />
 
-      <Route path='/' component={TopStoriesPage} />
-      <Route path='/article/:id' component={DetailPage} />
+      <Suspense fallback='Loading...'>
+        <Route path='/' component={TopStoriesPage} />
+        <Route path='/article/:id' component={DetailPage} />
+      </Suspense>
     </>
   )
 }
