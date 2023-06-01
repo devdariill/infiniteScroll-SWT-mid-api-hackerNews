ae9af72224650082a141e1bd282763e8a762ab06
nATOr <gowluonade@gmail.com>
2023-06-01 01:51:20 -0500

    _

---
+++ src/components/Story.tsx
@@ -0,0 +1,15 @@
+
+interface StoryProps {
+  id: number
+  index: number
+}
+
+export const Story = (props: StoryProps) => {
+  const { id, index } = props
+
+  return (
+    <div>
+      HOlis
+    </div>
+  )
+}
