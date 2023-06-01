fc4fe4be7ca349e4fb703a051fbe53bbfb41180f
nATOr <gowluonade@gmail.com>
2023-06-01 03:08:34 -0500

    swr

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,6 +1,17 @@
-function Detail () {
+import useSWR from 'swr'
+import ListOfComments from '../components/ListOfComments'
+import { getItemInfo } from '../services/hacker-news'
+
+function Detail (props: { params: { id: number } }) {
+  const { params: { id } } = props
+  // const { id } = props.params
+  const { data, isLoading } = useSWR(`/story/${id}`, () => getItemInfo(id))
   return (
-    <div>Detail</div>
+    <div className=''>
+      {
+        isLoading ? <div>Loading...</div> : <ListOfComments ids={data?.kids} />
+      }
+    </div>
   )
 }
 
