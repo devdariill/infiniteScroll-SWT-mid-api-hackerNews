b5245fbec0c3cf6f76a9c030ed607f6f04a83add
nATOr <gowluonade@gmail.com>
2023-06-01 03:59:59 -0500

    flat

--- src/pages/TopStories.tsx
+++ src/pages/TopStories.tsx
@@ -12,13 +12,19 @@ function TopStories () {
       return getTopStories(Number(page), 10)
     }
   )
+  // exmaple for flat()
+  // const data = [[1,2,3], [4,5,6], [7,8,9]] -> [1,2,3,4,5,6,7,8,9]
+  // const data = [[1,2,3], [4,5,6], [7,8,[9]]] -> [1,2,3,4,5,6,7,8,[9]]
+  // example for flat(2)
+  // const data = [[1,2,3], [4,5,6], [7,8,[9]]] -> [1,2,3,4,5,6,7,8,9]
+  const stories = data?.flat()
   // const { data, error, isLoading } = useSWR('stories', () => getTopStories(1, 10))
 
   return (
     <>
       {isLoading && <StoryLoader />}
       <ul style={{ listStyle: 'none' }}>
-        {data?.map((id: number, index: number) => (
+        {stories?.map((id: number, index: number) => (
           <li key={id}>
             <Story id={id} index={index} />
           </li>
