52db806a4c3576d3ede4fa3b5db67cef76a46aed
nATOr <gowluonade@gmail.com>
2023-06-01 01:52:05 -0500

    swr-fetch-2

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,3 +1,5 @@
+import useSWR from 'swr'
+import { getItemInfo } from '../services/hacker-news'
 
 interface StoryProps {
   id: number
@@ -6,10 +8,23 @@ interface StoryProps {
 
 export const Story = (props: StoryProps) => {
   const { id, index } = props
+  const { data, isLoading } = useSWR('stories', () => getItemInfo(id))
+
+  if (isLoading) {
+    return <>Loading...</>
+  }
+
+  const { by, kids, score, title, url } = data
+  console.log(data)
 
   return (
-    <div>
-      HOlis
-    </div>
+    <article className=''>
+      <header className=''>
+        <small className=''>{index}.</small>
+        <a className='' href={url} target='_black' rel='noopener noreferer'>{title}</a>
+        <a href={url}>({url})</a>
+      </header>
+      <footer />
+    </article>
   )
 }
