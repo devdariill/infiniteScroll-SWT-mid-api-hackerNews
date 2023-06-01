7af3233320b3744cb73d5e450c6cf464e8cf6df3
nATOr <gowluonade@gmail.com>
2023-06-01 01:18:28 -0500

    pagination

--- src/services/hacker-news.ts
+++ src/services/hacker-news.ts
@@ -1,7 +1,11 @@
-export const getTopStories = async () => {
+export const getTopStories = async (page: number, limit: number) => {
   const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
   const json = await response.json()
-  return json
+  // pagination
+  const startIndex = (page - 1) * limit
+  const endIndex = startIndex + limit
+  const ids = json.slice(startIndex, endIndex)
+  return ids
 }
 
 export const getItemInfo = async (id: number) => {
