2c14f9db9d25896ded11379262e6ef6e1b2fc556
nATOr <gowluonade@gmail.com>
2023-06-01 01:33:25 -0500

    Promise-all

--- src/services/hacker-news.ts
+++ src/services/hacker-news.ts
@@ -5,7 +5,7 @@ export const getTopStories = async (page: number, limit: number) => {
   const startIndex = (page - 1) * limit
   const endIndex = startIndex + limit
   const ids = json.slice(startIndex, endIndex)
-  return ids
+  return await Promise.all(ids.map(async (id: number) => await getItemInfo(id)))
 }
 
 export const getItemInfo = async (id: number) => {
