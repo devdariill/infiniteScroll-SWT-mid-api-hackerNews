760bad988b32fe09a0bdf7bd74b08adbe8b6aff6
nATOr <gowluonade@gmail.com>
2023-06-01 03:17:41 -0500

    swr-fetch-3

--- src/components/ListOfComments.tsx
+++ src/components/ListOfComments.tsx
@@ -1,9 +1,40 @@
+import useSWR from 'swr'
+import { getItemInfo } from '../services/hacker-news'
+
+const Comment = (props: { id: number }) => {
+  const { id } = props
+  const { data, isLoading } = useSWR(`/comments/${id}`, () => getItemInfo(id))
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  const { by, time, text, kids } = data
+
+  return (
+    <article>
+      <header>
+        <small>
+          <span>{by}</span>
+          <span>.</span>
+          <span>5 hours ago</span>
+        </small>
+      </header>
+      <p>{text}</p>
+    </article>
+  )
+}
+
 function ListOfComments (props: { ids: number[] }) {
   const { ids } = props
   return (
-    <>
-      {JSON.stringify(ids)}
-    </>
+    <ul>
+      {
+        ids?.map((id: number) => (
+          <li key={id}>
+            <Comment id={id} />
+          </li>
+        ))
+      }
+    </ul>
   )
 }
 
