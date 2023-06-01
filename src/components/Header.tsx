88d1180f2d4a16669bca42578a708836682f5f05
nATOr <gowluonade@gmail.com>
2023-06-01 00:36:47 -0500

    _

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+
+function Header () {
+  return (
+    <nav>
+      <img src='/logo.png' alt='logo' />
+      <a href='/'>
+        Hacker News
+      </a>
+    </nav>
+  )
+}
+
+export default Header
