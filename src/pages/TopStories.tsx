6b5fa4780fba45fee3607c34903be97d9cd6cd1c
nATOr <gowluonade@gmail.com>
2023-06-01 03:51:14 -0500

    swr/infinite

--- src/pages/TopStories.tsx
+++ src/pages/TopStories.tsx
@@ -1,21 +1,30 @@
-import useSWR from 'swr'
+// import useSWR from 'swr'
+import useSWRInfinite from 'swr/infinite'
 import { Story } from '../components/Story'
 import { StoryLoader } from '../components/StoryLoader'
 import { getTopStories } from '../services/hacker-news'
+
 function TopStories () {
-  const { data, error, isLoading } = useSWR('stories', () => getTopStories(1, 10))
+  const { data, isLoading } = useSWRInfinite(
+    (index) => `/stories/${index + 1}`,
+    (key) => {
+      const [,page] = key.split('/')
+      return getTopStories(Number(page), 10)
+    }
+  )
+  // const { data, error, isLoading } = useSWR('stories', () => getTopStories(1, 10))
 
   return (
     <>
+      {isLoading && <StoryLoader />}
       <ul style={{ listStyle: 'none' }}>
-        {isLoading && <StoryLoader />}
-        {error != null && <li>Something went wrong</li>}
         {data?.map((id: number, index: number) => (
           <li key={id}>
             <Story id={id} index={index} />
           </li>
         ))}
       </ul>
+      <button>Load More</button>
     </>
   )
 }
